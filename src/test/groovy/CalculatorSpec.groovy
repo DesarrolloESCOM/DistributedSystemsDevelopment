3220c2b7bac1a5666ceac0e12d6e777981e945f0
alberto <alberto@alberto-Ideapad-Z570.(none)>
2015-03-29 22:59:35 -0600

    Homework 2-4, updating git ignore

--- src/test/groovy/CalculatorSpec.groovy
+++ src/test/groovy/CalculatorSpec.groovy
@@ -7,29 +7,56 @@ import spock.lang.Shared
 class CalculatorSpec extends spock.lang.Specification {
     @Shared
     Calculator calc
-    def setupSpec(){
+
+    def setupSpec() {
         calc = new Calculator()
     }
+
     def "should sum n numbers"() {
         expect:
-            calc.sum(values) == result
+        calc.sum(values) == result
         where:
-            values     | result
-            [1,2,3]  | 6
-            [1,1,1]  | 3
-            []       | 0
-            null     | 0
-            [1,-1,-1]|-1
+        values      | result
+        [1, 2, 3]   | 6
+        [1, 1, 1]   | 3
+        []          | 0
+        null        | 0
+        [1, -1, -1] | -1
     }
-    def "should subtract to the first number the next n values"(){
+
+    def "should subtract to the first number the next n values"() {
         expect:
-            calc.subtract(values) == result
+        calc.subtract(values) == result
         where:
-            values     | result
-            [1,2,3]    | -4
-            [1,1,1]    | -1
-            []         | 0
-            null       | 0
-            [1,-1,-1]  | 3
+        values      | result
+        [1, 2, 3]   | -4
+        [1, 1, 1]   | -1
+        []          | 0
+        null        | 0
+        [1, -1, -1] | 3
+    }
+
+    def "should multiply all the vales from a list"() {
+        expect:
+        calc.multiply(values) == result
+        where:
+        values      | result
+        [1, 2, 3]   | 6
+        [1, 1, 1]   | 1
+        []          | 0
+        null        | 0
+        [1, -1, -1] | 1
+    }
+
+        def "should divide the first value from the next n  vales from a list"() {
+        expect:
+        calc.multiply(values) == result
+        where:
+        values      | result
+        [1, 2, 3]   | 6
+        [1, 1, 1]   | 1
+        []          | 0
+        null        | 0
+        [1, -1, -1] | 1
     }
 }
